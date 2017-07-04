1e2f9c1266a63101c7bc773d326270e153967734
Darren Wiens <dkwiens@gmail.com>
2017-07-04 12:09:58 -0700

    clean up console

--- src/app/storymaps/tpl/core/Maptiks.js
+++ src/app/storymaps/tpl/core/Maptiks.js
@@ -4,7 +4,6 @@ define(["dojo/topic"],
       require(['maptiks'], function (mapWrapper) {
         if (!app.map.maptiks) {
           var container = app.map.container; // the current map div
-          console.log(app.data.getWebAppData().getMaptiks());
           var maptiksSet = app.data.getWebAppData().getMaptiks().hasOwnProperty('maptiksTrackcode');
           var maptiksTrackcode = maptiksSet ? app.data.getWebAppData().getMaptiks().maptiksTrackcode : 'c311cf16-ad79-42b1-97f9-f433be6c8b00'; // replace with dev trackcode
           var maptiksId = maptiksSet ? app.data.getWebAppData().getMaptiks().maptiksId : "demo";
