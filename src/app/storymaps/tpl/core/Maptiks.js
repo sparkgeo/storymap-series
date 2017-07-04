d2e49875458cfca09bef92e48eb7dd3d97d624f4
Darren Wiens <dkwiens@gmail.com>
2017-07-04 16:39:00 -0700

    quotes

--- src/app/storymaps/tpl/core/Maptiks.js
+++ src/app/storymaps/tpl/core/Maptiks.js
@@ -1,16 +1,16 @@
 define(["dojo/topic"],
   function(topic){
     topic.subscribe("story-loaded-map", function(){
-      require(['maptiks'], function (mapWrapper) {
+      require(["maptiks"], function (mapWrapper) {
         if (!app.map.maptiks) {
           var container = app.map.container; // the current map div
-          var maptiksSet = app.data.getWebAppData().getMaptiks().hasOwnProperty('maptiksTrackcode');
-          var maptiksTrackcode = maptiksSet ? app.data.getWebAppData().getMaptiks().maptiksTrackcode : 'c311cf16-ad79-42b1-97f9-f433be6c8b00'; // replace with dev trackcode
+          var maptiksSet = app.data.getWebAppData().getMaptiks().hasOwnProperty("maptiksTrackcode");
+          var maptiksTrackcode = maptiksSet ? app.data.getWebAppData().getMaptiks().maptiksTrackcode : "c311cf16-ad79-42b1-97f9-f433be6c8b00"; // replace with dev trackcode
           var maptiksId = maptiksSet ? app.data.getWebAppData().getMaptiks().maptiksId : "demo";
           var maptiksMapOptions = {
             extent: app.map.extent,
             maptiks_trackcode: maptiksTrackcode,
-            maptiks_id: maptiksId + ':' + app.data.getCurrentEntry().title
+            maptiks_id: maptiksId + ":" + app.data.getCurrentEntry().title
           };
           mapWrapper(container, maptiksMapOptions, app.map);
           topic.publish("maptiks-ready");
