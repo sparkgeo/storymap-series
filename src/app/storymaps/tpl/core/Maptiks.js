c20a4e79db80384261538fd3644fed202edc4d9b
Darren Wiens <dkwiens@gmail.com>
2017-07-04 12:06:05 -0700

    demo in iframe, maptiks in own js file

--- src/app/storymaps/tpl/core/Maptiks.js
+++ src/app/storymaps/tpl/core/Maptiks.js
@@ -1,41 +1,20 @@
 define(["dojo/topic"],
   function(topic){
-    if (window.location.pathname.endsWith("demo.html")) {
-      var demo = true;
-      var Lmap = L.map("maptiks-map");
-    }
-    topic.subscribe("story-loaded-map", function () {
-      require(["maptiks"], function (mapWrapper) {
-        var container = app.map.container; // the current map div
-        var maptiksMapOptions = {
-          extent: app.map.extent,
-          maptiks_trackcode: demo ? "c311cf16-ad79-42b1-97f9-f433be6c8b00" : app.data.getWebAppData().getMaptiks().maptiksTrackcode,
-          maptiks_id: demo ? "test:" + app.data.getCurrentEntry().title : app.data.getWebAppData().getMaptiks().maptiksId + ":" + app.data.getCurrentEntry().title  // from Builder Maptiks settings, ID:tabname
-        };
-        
+    topic.subscribe("story-loaded-map", function(){
+      require(['maptiks'], function (mapWrapper) {
         if (!app.map.maptiks) {
+          var container = app.map.container; // the current map div
+          console.log(app.data.getWebAppData().getMaptiks());
+          var maptiksSet = app.data.getWebAppData().getMaptiks().hasOwnProperty('maptiksTrackcode');
+          var maptiksTrackcode = maptiksSet ? app.data.getWebAppData().getMaptiks().maptiksTrackcode : 'c311cf16-ad79-42b1-97f9-f433be6c8b00'; // replace with dev trackcode
+          var maptiksId = maptiksSet ? app.data.getWebAppData().getMaptiks().maptiksId : "demo";
+          var maptiksMapOptions = {
+            extent: app.map.extent,
+            maptiks_trackcode: maptiksTrackcode,
+            maptiks_id: maptiksId + ':' + app.data.getCurrentEntry().title
+          };
           mapWrapper(container, maptiksMapOptions, app.map);
-        }
-        
-        if (demo) {
-          var esriExtent = app.map.geographicExtent;
-          var leafletExtent = L.latLngBounds([L.latLng(esriExtent.ymin, esriExtent.xmin),L.latLng(esriExtent.ymax, esriExtent.xmax)]);
-
-          Lmap.fitBounds(leafletExtent);
-
-          L.esri.basemapLayer("Gray").addTo(Lmap);
-
-          app.map.maptiks.off("maptiks:track");
-          app.map.maptiks.on("maptiks:track", function (e) {
-            console.log(e); // prints maptiks parameters
-            var position = e.hasOwnProperty("position") ? [e.position[1], e.position[0]] : [e.map_center[1], e.map_center[0]];
-
-            var marker = L.marker(position).addTo(Lmap);
-            var contentString = '<div id="content">'+
-              "<h1>" + e.action + "</h1><br>" +
-              "<pre>" + JSON.stringify(e, null, 2) + "</pre>" + "</div>";
-            marker.bindPopup(contentString).openPopup();
-          });
+          topic.publish("maptiks-ready");
         }
       });
     });
