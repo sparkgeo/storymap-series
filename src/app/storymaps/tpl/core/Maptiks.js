107a33af242a3d84ed10659369b1d52db2e45d22
Darren Wiens <dkwiens@gmail.com>
2017-07-05 14:02:33 -0700

    Move demo maptiks code to demo.html. Check if maptiks already tracking map, and if maptiks parameters have been set in builder.

--- src/app/storymaps/tpl/core/Maptiks.js
+++ src/app/storymaps/tpl/core/Maptiks.js
@@ -2,18 +2,16 @@ define(["dojo/topic"],
   function(topic){
     topic.subscribe("story-loaded-map", function(){
       require(["maptiks"], function (mapWrapper) {
-        if (!app.map.maptiks) {
-          var container = app.map.container; // the current map div
-          var maptiksSet = app.data.getWebAppData().getMaptiks().hasOwnProperty("maptiksTrackcode");
-          var maptiksTrackcode = maptiksSet ? app.data.getWebAppData().getMaptiks().maptiksTrackcode : "c311cf16-ad79-42b1-97f9-f433be6c8b00"; // replace with dev trackcode
-          var maptiksId = maptiksSet ? app.data.getWebAppData().getMaptiks().maptiksId : "demo";
-          var maptiksMapOptions = {
-            extent: app.map.extent,
-            maptiks_trackcode: maptiksTrackcode,
-            maptiks_id: maptiksId + ":" + app.data.getCurrentEntry().title
-          };
-          mapWrapper(container, maptiksMapOptions, app.map);
-          topic.publish("maptiks-ready");
+        if (!app.map.maptiks) { // maptiks are not yet tracking this map
+          if (app.data.getWebAppData().getMaptiks().maptiksTrackcode) { // maptiks have been set in builder
+            var container = app.map.container; // the current map div
+            var maptiksMapOptions = {
+              maptiks_trackcode: app.data.getWebAppData().getMaptiks().maptiksTrackcode, // from Builder Maptiks settings
+              maptiks_id: app.data.getWebAppData().getMaptiks().maptiksId + ":" + app.data.getCurrentEntry().title // from Builder Maptiks settings, ID:tabname
+            };
+            mapWrapper(container, maptiksMapOptions, app.map);
+          }
+          topic.publish("demo-ready", mapWrapper);
         }
       });
     });
